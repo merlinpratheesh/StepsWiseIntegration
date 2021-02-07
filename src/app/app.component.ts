9a4b79f84fa5a155fed3df8d6af647d14a85e226
gmanojisaac <gmanoj.isaac@gmail.com>
2021-02-07 10:31:36 +0530

    DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,OnInit } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
+import firebase from 'firebase/app';
+import '@firebase/auth';
+
+import { auth as uiAuth } from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Goldenfile';
+export class AppComponent implements AfterViewInit {
+  constructor(public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
+    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+  }
+  ngAfterViewInit(): void {
+
+  }
+  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
+    console.log('successCallback', data);
+
+  }
+
+  errorCallback(data: FirebaseUISignInFailure) {
+    console.warn('errorCallback', data);
+  }
+
+  uiShownCallback() {
+    console.log('UI shown');
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,23 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment'
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInOptions: [
+      {
+        // Google provider must be enabled in Firebase Console to support one-tap
+        // sign-up.
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // Required to enable ID token credentials for this provider.
+        // This can be obtained from the Credentials page of the Google APIs
+        // console. Use the same OAuth client ID used for the Google provider
+        // configured with GCIP or Firebase Auth.
+        clientId: '155833140934-om6lcebvnosmo8sgkkogpojvs74g0la5.apps.googleusercontent.com'
+      }],
+
+    credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+
+  };
 
 
 @NgModule({
@@ -25,6 +42,8 @@ import { environment } from '../environments/environment'
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
     AngularFireStorageModule // storage
+    ,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
